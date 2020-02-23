e36da786f2b1961c6264619f876d1ac7bd83d587
ijgnd <fooijgnd@barijgnd>
2020-02-23 11:46:00 +0100

    python3 and my editor default to utf8

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,1 @@
-# -*- coding: utf-8 -*-
-# required for this directory to be recognized as a package
+from . import organizer
--- src/rearranger.py
+++ src/rearranger.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """
 This file is part of the Note Organizer add-on for Anki
 
